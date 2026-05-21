d01887c9fba07f572564d72f9a37371ba7a67beb
begin2019510 <begin2019510@users.noreply.github.com>
2026-05-21 10:35:12 +0800

    feat: v2.5.1 更新日志弹窗 - 点击版本号查看历史变更

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 + 推送通知
-const CACHE = 'purchase-cache-v7';
+const CACHE = 'purchase-cache-v8';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
