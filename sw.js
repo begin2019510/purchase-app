42f9a610bfc2714cb8de76f367031cc8b1abeb24
begin2019510 <begin2019510@users.noreply.github.com>
2026-05-24 23:20:06 +0800

    chore: SW cache v26

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 + 推送通知
-const CACHE = 'purchase-cache-v25';
+const CACHE = 'purchase-cache-v26';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/help'];
 
 self.addEventListener('install', e => {
